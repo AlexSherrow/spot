c8431338e94601548d4a8b07c80d84849ff4036e
AlexSherrow <sherrowa226@gmail.com>
2023-04-19 22:48:20 -0400

    FrontEnd

--- src/layouts/HomePage/HomePage.jsx
+++ src/layouts/HomePage/HomePage.jsx
@@ -1,5 +1,5 @@
 export const HomePage = () => {
-  let devURL = "https://alexsherrowspotify.herokuapp.com";
+  let devURL = "http://www.soundslounge.com";
   //let devURL = "http://localhost:3000";
   return (
     <>
